888b3c14c4b98fb19635f7bc9e4e69e4d980036a
sylvain <fraisse.sylvain@gmail.com>
2024-02-01 15:39:46 +0100

    :sparkles: create a github action

--- index.ts
+++ index.ts
@@ -1,1 +1,18 @@
-console.log('Hello world!')
+import * as core from '@actions/core'
+
+import { synchronizeBranchesAndLabels } from "./src"
+
+export async function run(): Promise<void> {
+  try {
+    const repository: string[] = core.getInput('repository').split('/');
+    const sourceBranch: string = core.getInput('source-branch');
+    const targetBranch: string = core.getInput('target-branch');
+    const label: string = core.getInput('label');
+
+    await synchronizeBranchesAndLabels({ repository: { owner: repository[0], repo: repository[1] }, sourceBranch, targetBranch, label })
+  } catch (error: any) {
+    core.setFailed(error.message);
+  }
+}
+
+await run()
