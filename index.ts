3f97abcefac9ebc6a3e6f70977a63487cbbd3761
sylvain <fraisse.sylvain@gmail.com>
2024-02-02 10:42:32 +0100

    :bug: action attributes are empty

--- index.ts
+++ index.ts
@@ -4,10 +4,10 @@ import { synchronizeBranchesAndLabels } from "./src"
 
 export async function run(): Promise<void> {
   try {
-    const repository: string[] = core.getInput('repository').split('/');
-    const sourceBranch: string = core.getInput('source-branch');
-    const targetBranch: string = core.getInput('target-branch');
-    const label: string = core.getInput('label');
+    const repository: string[] = core.getInput('repository', { required: true }).split('/');
+    const sourceBranch: string = core.getInput('source-branch', { required: true });
+    const targetBranch: string = core.getInput('target-branch', { required: true });
+    const label: string = core.getInput('label', { required: true });
 
     await synchronizeBranchesAndLabels({ repository: { owner: repository[0], repo: repository[1] }, sourceBranch, targetBranch, label })
   } catch (error: any) {
